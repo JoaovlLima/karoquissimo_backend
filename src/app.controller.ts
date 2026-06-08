7adf0c48f87e86f61dab6dd907ca4df83263efaf
JoaovlLima <jl949615@gmail.com>
2026-06-08 10:52:15 -0300

    add helth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,3 +10,14 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+@Controller('health')
+export class HealthController {
+  @Get()
+  health() {
+    return {
+      status: 'UP',
+      timestamp: new Date().toISOString(),
+    };
+  }
+}
